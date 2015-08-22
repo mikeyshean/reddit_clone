2603fad98d96aa3012b693041150744f9060f1c9
Mikey Shean <mdshean2@gmail.com>
2015-08-21 22:31:42 -0400

    Add Subs controller & views.

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,5 +1,58 @@
 class SubsController < ApplicationController
 
+  def index
+    @subs = Sub.all
+  end
 
+  def new
+    @sub = Sub.new
+    render :new
+  end
+
+  def create
+    @sub = current_user.subs.new(sub_params)
+
+    if @sub.save
+      flash[:notices] = ["Successfully created #{@sub.title}!"]
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+
+    if @sub.update(sub_params)
+      flash[:notices] = ["Successfully updated #{@sub.title}!"]
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+    render :show
+  end
+
+  def destroy
+    @sub = Sub.find(params[:id])
+    @sub.destroy
+    redirect_to subs_url
+  end
+
+
+  private
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
 
 end
